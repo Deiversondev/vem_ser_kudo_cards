b8d1ea1691c5a5017b43288731b2f86509721e0d
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-12-17 13:15:46 -0300

    Serviço de email

--- retrocards/src/Pages/Emails.js
+++ retrocards/src/Pages/Emails.js
@@ -6,12 +6,23 @@ function Emails (){
   
   const[email, setEmail]= useState('')
   const[lista, setLista]= useState([])
+  
 
 
   const cadastrarEmail =() => {
-
     setLista([...lista, (email)])
     
+  }
+
+  
+
+  
+
+  const deleteEmail = (ff, e) =>{
+    // e.preventDefault();
+     setLista(lista.filter((email) => email !== ff))
+    
+      console.log(lista)
   }
 
 
@@ -40,17 +51,31 @@ function Emails (){
   return(
     
     <div >
+
+     
       <h1>Página emails</h1>
+      {
+        lista && lista.map(em =>(
+         <div>
+            <p>{em}   <button onClick={() => deleteEmail(em)}>Remove</button></p>
+        
+         </div>
+
+        ))
+      }
       <form>
+     
         <div>
-          <input type="email" onChange={e => setEmail(e.target.value)}/>
+          <input type="text" onChange={e => setEmail(e.target.value)}/>
         </div>
         <div>
           <small>Você pode adicionar múltiplos e-mails usando vírgula(',')</small>
         </div>
+        
         <button type="button" onClick={()=> cadastrarEmail()} >Add</button>
         <button type="submit" onClick={()=> enviarEmail()} >Enviar</button>
       </form>
+     
     </div>
   );
 }
