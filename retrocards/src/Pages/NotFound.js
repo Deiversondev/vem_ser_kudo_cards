1fcd47ddf2a0852f5bbb457446b6d2b9ca7a27c6
Ana <bialima.150324@gmail.com~>
2021-12-17 11:55:25 -0300

    Rotas privadas e Not Found

---
+++ retrocards/src/Pages/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = ()=>{
+  return(
+    <div className='containerContent'>
+      <h1>Page Not found</h1>
+    </div>
+  )
+}
+
+export default NotFound;
--- retrocards/src/router.js
+++ retrocards/src/router.js
@@ -5,7 +5,8 @@ import {
   Navigate
   
 } from 'react-router-dom';
-
+import { useContext, useEffect } from 'react';
+import { AuthContext } from './context/AuthContext';
 import CreateSprint from './Pages/CreateSprint';
 import CreateRetrospectiva from './Pages/CreateRetrospectiva';
 import CreateKudoCards from './Pages/CreateKudoCards';
@@ -18,27 +19,51 @@ import Retrospectiva from './Pages/Retrospectiva';
 import KudoBox from './Pages/KudoBox';
 import KudoCard from './Pages/KudoCard';
 import Emails from './Pages/Emails';
+import NotFound from './Pages/NotFound';
 
 
 function Routers (){
+  const{auth, setAuth} = useContext(AuthContext)
+
+  const localToken = localStorage.getItem('token')
+  
+  useEffect (()=>{
+
+    if (localToken){
+      setAuth(true)
+    }
+  
+    // if (auth.loading){
+    //   return <Loading/>
+    // }
+  },[])
+
   return(
-  <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Navigate to='/login'/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/initialpage' element={<InitialPage/>}/>
-    <Route path='/createsprint' element={<CreateSprint/>}/>
-    <Route path='/sprint' element={<Sprint/>}/>
-    <Route path='/kudobox' element={<KudoBox/>}/>
-    <Route path='/kudocard' element={<KudoCard/>}/>
-    <Route path='/createretrospectiva' element={<CreateRetrospectiva/>}/>
-    <Route path='/createkudocards' element={<CreateKudoCards/>}/>
-    <Route path='/createkudobox' element={<CreateKudoBox/>}/>
-    <Route path='/createitems' element={<CreateItems/>}/>
-    <Route path='/retrospectiva' element={<Retrospectiva/>}/>
-    <Route path='/emails' element={<Emails/>}/>
-    </Routes>
-  </BrowserRouter>    
+    <BrowserRouter>
+      {auth? 
+      <Routes>
+        <Route path='/initialpage' element={<InitialPage/>}/>
+        <Route path='/createsprint' element={<CreateSprint/>}/>
+        <Route path='/sprint' element={<Sprint/>}/>
+        <Route path='/kudobox' element={<KudoBox/>}/>
+        <Route path='/kudocard' element={<KudoCard/>}/>
+        <Route path='/createretrospectiva' element={<CreateRetrospectiva/>}/>
+        <Route path='/createkudocards' element={<CreateKudoCards/>}/>
+        <Route path='/createkudobox' element={<CreateKudoBox/>}/>
+        <Route path='/createitems' element={<CreateItems/>}/>
+        <Route path='/retrospectiva' element={<Retrospectiva/>}/>
+        <Route path='/emails' element={<Emails/>}/>
+        <Route path='/*' element={<NotFound/>}/>
+      </Routes>
+      :
+      <Routes>
+        <Route path='/' element={<login/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/*' element={<NotFound/>}/>
+      </Routes>
+      }
+      
+    </BrowserRouter>    
 
   )
 }
