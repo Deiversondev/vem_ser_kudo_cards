0fc62b1520ddf2496f15f376418408a780237402
Ana <bialima.150324@gmail.com~>
2021-12-17 17:57:38 -0300

    today moment email

--- retrocards/src/Pages/email/Emails.js
+++ retrocards/src/Pages/email/Emails.js
@@ -1,22 +1,25 @@
-import {  useState } from "react";
+import {  useState, useContext } from "react";
 import api from "../../api";
+import { AuthContext } from '../../context/AuthContext'
+import Loading from '../../components/loading/Loading'
+import moment from "moment";
 
 
 function Emails (){
   
   const[email, setEmail]= useState('')
   const[lista, setLista]= useState([])
-  
+  const{loading, setLoading}= useContext(AuthContext)
 
+  const moment = require('moment');
+  const today = moment();
+  
 
   const cadastrarEmail =() => {
     setLista([...lista, (email)])
     
   }
 
-  
-
-  
 
   const deleteEmail = (ff, e) =>{
     // e.preventDefault();
@@ -29,15 +32,18 @@ function Emails (){
   const enviarEmail = async() => {
   
     const report = {
-      assunto: "string",
-      dataEnvio: "2021-12-16",
+      assunto: "Relatório Retrspectiva",
+      dataEnvio: today.format('YYYY-MM-DD'),
       emailDestinatario: lista.toString()
     }
    
 
     console.log(lista, report)
+    
   try{
+    setLoading(true)
     const {data} =  await api.post('/email', report)
+    setLoading(false)
   } catch(e){
     console.log(e)
   }
@@ -51,31 +57,33 @@ function Emails (){
   return(
     
     <div >
+      {loading && <Loading/>}
+      {!loading && 
+      <div>
+        <h1>Página emails</h1>
+        {
+          lista && lista.map(em =>(
+          <div>
+              <p>{em}   <button onClick={() => deleteEmail(em)}>Remove</button></p>
+          
+          </div>
 
-     
-      <h1>Página emails</h1>
-      {
-        lista && lista.map(em =>(
-         <div>
-            <p>{em}   <button onClick={() => deleteEmail(em)}>Remove</button></p>
-        
-         </div>
-
-        ))
+          ))
+        }
+        <form>
+      
+          <div>
+            <input type="text" onChange={e => setEmail(e.target.value)}/>
+          </div>
+          <div>
+            <small>Você pode adicionar múltiplos e-mails usando vírgula(',')</small>
+          </div>
+          
+          <button type="button" onClick={()=> cadastrarEmail()} >Add</button>
+          <button type="submit" onClick={()=> enviarEmail()} >Enviar</button>
+        </form>
+      </div>
       }
-      <form>
-     
-        <div>
-          <input type="text" onChange={e => setEmail(e.target.value)}/>
-        </div>
-        <div>
-          <small>Você pode adicionar múltiplos e-mails usando vírgula(',')</small>
-        </div>
-        
-        <button type="button" onClick={()=> cadastrarEmail()} >Add</button>
-        <button type="submit" onClick={()=> enviarEmail()} >Enviar</button>
-      </form>
-     
     </div>
   );
 }
