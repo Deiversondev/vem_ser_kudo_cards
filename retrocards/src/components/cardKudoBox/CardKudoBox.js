464eafcd41f26950ae9ac00f3f752a1c8d23f46d
Ana <bialima.150324@gmail.com~>
2021-12-19 11:54:01 -0300

    confirmação envio de e-mail e Botão iniciar Kudo box

--- retrocards/src/components/cardKudoBox/CardKudoBox.js
+++ retrocards/src/components/cardKudoBox/CardKudoBox.js
@@ -1,32 +1,51 @@
 import { useContext } from "react";
+import api from "../../api";
 import { ListContext } from "../../context/ListContext"
+import { AuthContext } from "../../context/AuthContext";
+import Loading from "../loading/Loading";
 
 
 
 function CardKudoBox (){
 
   const{listKudoBoxes} = useContext(ListContext)
+  const{loading, setLoading}= useContext(AuthContext)
   
   const goToKudoBox = (idKudoBox)=> {
     localStorage.setItem('idKudoBox', idKudoBox)
     window.location.href='/kudobox'
   }
 
+  const startKudoBox = async(idKudoBox)=> {
+    
+    setLoading(true)
+    await api.put(`/kudobox/${idKudoBox}/status?status=EM_ANDAMENTO`)
+    setLoading(false)
+
+    window.location.reload()
+  }
+
   return (
     <div>
-      <ul>
-        {listKudoBoxes.map(kudoBox => (
-          <div key={kudoBox.idKudoBox}>
-            <li>
-            {kudoBox.idKudoBox}
-            {kudoBox.titulo}
-            {kudoBox.statusKudoBoxEntity}
-            {kudoBox.dataLeitura}
-            <button onClick={()=> goToKudoBox(kudoBox.idKudoBox)}>Abrir Kudo Box</button>
-            </li>
-          </div>
-        ))}
-      </ul>
+      {loading && <Loading/>}
+      {!loading && 
+      <div>
+        <ul>
+          {listKudoBoxes.map(kudoBox => (
+            <div key={kudoBox.idKudoBox}>
+              <li>
+              {kudoBox.idKudoBox}
+              {kudoBox.titulo}
+              {kudoBox.statusKudoBoxEntity}
+              {kudoBox.dataLeitura}
+              <button onClick={()=> startKudoBox(kudoBox.idKudoBox)}>Iniciar Kudo Box</button>
+              <button onClick={()=> goToKudoBox(kudoBox.idKudoBox)}>Abrir Kudo Box</button>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
+      }
     </div>
   )
 }
