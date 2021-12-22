cd2c9085b3b88df300a979cb406b147afcd73f9a
Ana <bialima.150324@gmail.com~>
2021-12-21 22:17:19 -0300

    estilo card retrospectiva mais recente

--- retrocards/src/components/cardKudoCard/CardKudoCardsUsers.js
+++ retrocards/src/components/cardKudoCard/CardKudoCardsUsers.js
@@ -28,11 +28,11 @@ function CardKudoCardsUsers (){
           {listKudoCardsUser.map(kudoCard => (
             <div key={kudoCard.idKudoCard} className={styles.cardUser}>
             <li>
-            <p><strong>Id:</strong>{kudoCard.idKudoCard}</p>
-            <p><strong>Título:</strong>{kudoCard.titulo}</p>
-            <p><strong>Para:</strong>{kudoCard.para}</p>
-            <p><strong>Descrição:</strong>{kudoCard.descricao}</p>
-            <p><strong>KudoBox:</strong>{kudoCard.kudoBoxDTO.idKudoBox}</p>
+            <p><strong>Id: </strong>{kudoCard.idKudoCard}</p>
+            <p><strong>Título: </strong>{kudoCard.titulo}</p>
+            <p><strong>Para: </strong>{kudoCard.para}</p>
+            <p><strong>Descrição: </strong>{kudoCard.descricao}</p>
+            <p><strong>KudoBox: </strong>{kudoCard.kudoBoxDTO.idKudoBox}</p>
             <p>{kudoCard.kudoBoxDTO.titulo}</p>
             <p>{kudoCard.kudoBoxDTO.dataLeitura}</p>
             { 
